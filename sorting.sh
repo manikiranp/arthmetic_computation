0d88ccacd4e78fba9162ceafd950333da0608716
manikiranp <manikiranp3@gmail.com>
2020-10-10 21:56:04 +0530

    fourth arthmetic operation

--- sorting.sh
+++ sorting.sh
@@ -5,8 +5,10 @@ read -p "Enter three numbers: " a b c
 ar1=$(($a+$b*$c))
 ar2=$(($a*$b+$c))
 ar3=`perl -E "say $c+$a/$b"`
+ar4=`perl -E "say $a%$b+$c"`
 
 echo "$a+$b*$c = $ar1"
 echo "$a*$b+$c = $ar2"
 echo "$c+$a/$b = $ar3"
+echo "$a%$b+$c = $ar4"
 
