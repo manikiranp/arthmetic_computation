8d3bcdce3f7cf4a387d318d209296caa56ebde2d
manikiranp <manikiranp3@gmail.com>
2020-10-11 10:47:40 +0530

    storing values in dictionary

--- sorting.sh
+++ sorting.sh
@@ -2,13 +2,14 @@
 
 echo "Arthmetic Computation & Sorting"
 read -p "Enter three numbers: " a b c
-ar1=$(($a+$b*$c))
-ar2=$(($a*$b+$c))
-ar3=`perl -E "say $c+$a/$b"`
-ar4=`perl -E "say $a%$b+$c"`
+declare -A compute
+compute[r1]=$(($a+$b*$c))
+compute[r2]=$(($a*$b+$c))
+compute[r3]=`perl -E "say $c+$a/$b"`
+compute[r4]=`perl -E "say $a%$b+$c"`
 
-echo "$a+$b*$c = $ar1"
-echo "$a*$b+$c = $ar2"
-echo "$c+$a/$b = $ar3"
-echo "$a%$b+$c = $ar4"
+echo "$a+$b*$c = ${compute[r1]}"
+echo "$a*$b+$c = ${compute[r2]}"
+echo "$c+$a/$b = ${compute[r3]}"
+echo "$a%$b+$c = ${compute[r4]}"
 
